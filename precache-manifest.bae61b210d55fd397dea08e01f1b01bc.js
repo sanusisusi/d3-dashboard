e0d265e056f72e393fa3ea2d312bf3515458623c
Sanusi Abdulkadir <sanusiabdulkadir00@gmail.com>
2022-02-05 23:26:23 +0100

    Updates

---
+++ precache-manifest.bae61b210d55fd397dea08e01f1b01bc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2a75424178fc1a362bde3d8a8fec83",
+    "url": "/d3-dashboard/index.html"
+  },
+  {
+    "revision": "e13916ba7c1a8ea3e68a",
+    "url": "/d3-dashboard/static/js/2.c0ddc2a4.chunk.js"
+  },
+  {
+    "revision": "06d22ae6860a4e368490",
+    "url": "/d3-dashboard/static/js/main.d6fe5a9c.chunk.js"
+  },
+  {
+    "revision": "59aa3600982ff6875cad",
+    "url": "/d3-dashboard/static/js/runtime~main.14060033.js"
+  }
+]);
